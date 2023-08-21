580cdee6c44ddb1f56ba2f9e6d3f708685b0da2e
Michelle Aurelia <mkurniaw@sfu.ca>
2023-08-20 17:07:38 -0700

    more interactions

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function toggleNav() {
-    var hiddenItems = document.querySelectorAll(".hidden");
-    for (var i = 0; i < hiddenItems.length; i++) {
-      if (hiddenItems[i].style.display === "none") {
-        hiddenItems[i].style.display = "block";
-      } else {
-        hiddenItems[i].style.display = "none";
-      }
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeInElements = document.querySelectorAll(".fade-in");
+
+  function checkFadeIn() {
+      fadeInElements.forEach(element => {
+          const elementTop = element.getBoundingClientRect().top;
+          const windowHeight = window.innerHeight;
+
+          if (elementTop < windowHeight * 0.8) {
+              element.classList.add("fade-in-active");
+          }
+      });
   }
+
+  // Initial check when the page loads
+  checkFadeIn();
+
+  // Check when scrolling
+  window.addEventListener("scroll", checkFadeIn);
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var overlay = document.querySelector(".overlay");
+  setTimeout(function() {
+      overlay.style.opacity = 0;
+  }, 1000);
+});
